d9590ec6387afe0a7d9981819801cbf556ee506c
1239092095 <1239092095@qq.com>
2021-01-17 12:33:46 +0800

    n

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,37 +1,16 @@
-package com.example.demo;
+package com.njxz.demo;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-
-import org.springframework.beans.factory.InitializingBean;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import com.example.demo.bean.Article;
-
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
-public class DemoApplication implements InitializingBean{
+@MapperScan("com.njxz.demo.mapper")
+@ServletComponentScan
+public class DemoApplication {
 
-	
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
-
-	@Override
-	public void afterPropertiesSet() throws Exception {
-//		Article article = new Article();
-//		article.setArticleName("zzq");
-//		article.setCreatetime(new Date());
-//		article.setId(1);
-//		articleRep.save(article);
-		List<Article> list = new ArrayList<>();
-		while(true){
-			Article article = new Article();
-			list.add(article);
-		}
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 }
